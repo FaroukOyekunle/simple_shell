67b33ebfb649acda12108b5c2002c81800350adc
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-26 15:59:16 +0100

    Update puts.c

--- puts.c
+++ puts.c
@@ -1,1 +1,18 @@
+#include "shell.h"
 
+/**
+ * _puts - print every other character starting from first one
+ * @str: string passed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
